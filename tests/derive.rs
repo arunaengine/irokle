0ce1205cd406b5fb9e2feeb6cf806fc419e370ed
Sebastian Beyvers <sb@pus.de>
2026-05-20 14:49:12 +0200

    feat: Add irokle-derive

---
+++ tests/derive.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Eq, irokle::Event, Serialize, Deserialize)]
+#[irokle(type_id = "test.derive.note")]
+struct Note {
+    text: String,
+}
+
+#[test]
+fn derive_sets_type_id_and_uses_postcard_defaults() {
+    let note = Note {
+        text: "hello".into(),
+    };
+
+    let envelope = irokle::EventEnvelope::encode_event(&note).unwrap();
+    let decoded = envelope.decode_event::<Note>().unwrap();
+
+    assert_eq!(<Note as irokle::Event>::TYPE_ID, "test.derive.note");
+    assert_eq!(decoded, note);
+}
+
+#[test]
+fn derive_supports_renamed_crate_path() {
+    use irokle as renamed_irokle;
+
+    #[derive(Clone, Debug, PartialEq, Eq, renamed_irokle::Event, Serialize, Deserialize)]
+    #[irokle(type_id = "test.derive.renamed", crate = "renamed_irokle")]
+    struct Renamed {
+        value: u32,
+    }
+
+    let envelope = renamed_irokle::EventEnvelope::encode_event(&Renamed { value: 7 }).unwrap();
+    let decoded = envelope.decode_event::<Renamed>().unwrap();
+
+    assert_eq!(decoded.value, 7);
+}
