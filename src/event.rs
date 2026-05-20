45dfdfa56813c3c5dc03efd5e69ceb73adfd6cb2
Sebastian Beyvers <sb@pus.de>
2026-05-20 13:46:47 +0200

    feat: define event

---
+++ src/event.rs
@@ -0,0 +1,45 @@
+use bytes::Bytes;
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
+
+use crate::Result;
+
+pub trait Event: Serialize + DeserializeOwned + Sized + Send + Sync + 'static {
+    const TYPE_ID: &'static str;
+
+    fn encode(&self) -> Result<Bytes> {
+        Ok(Bytes::from(postcard::to_allocvec(self)?))
+    }
+
+    fn decode(bytes: &[u8]) -> Result<Self> {
+        postcard::from_bytes(bytes).map_err(Into::into)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct EventEnvelope {
+    pub type_id: String,
+    pub payload: Bytes,
+}
+
+impl EventEnvelope {
+    pub fn new<E: Event>(payload: Bytes) -> Self {
+        Self {
+            type_id: E::TYPE_ID.to_owned(),
+            payload,
+        }
+    }
+
+    pub fn encode_event<E: Event>(event: &E) -> Result<Self> {
+        Ok(Self::new::<E>(event.encode()?))
+    }
+
+    pub fn decode_event<E: Event>(&self) -> Result<E> {
+        if self.type_id != E::TYPE_ID {
+            return Err(crate::Error::EventTypeMismatch {
+                expected: E::TYPE_ID.to_owned(),
+                actual: self.type_id.clone(),
+            });
+        }
+        E::decode(&self.payload)
+    }
+}
